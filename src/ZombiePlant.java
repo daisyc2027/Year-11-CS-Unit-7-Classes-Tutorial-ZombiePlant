f531b983019f9fea3de28778420b58f4ad2117f3
daisyc2027 <daisyc2027@student.cis.edu.hk>
2025-01-22 12:48:23 +0800

    finished

---
+++ src/ZombiePlant.java
@@ -0,0 +1,37 @@
+public class ZombiePlant {
+    private int potency;
+    private boolean isDangerous = true;
+    private int treatmentsNeeded;
+
+    public ZombiePlant(int maxPotency, int initialTreatmentsNeeded){
+        this.potency = maxPotency;
+        this.treatmentsNeeded = initialTreatmentsNeeded;
+    }
+
+    public int getPotency(){
+        return potency;
+    }
+
+    public int treatmentsNeeded(){
+        return treatmentsNeeded;
+    }
+
+    public boolean isDangerous(){
+        return isDangerous;
+    }
+
+    public void treat(int treatment){
+        if(treatment > 0){
+            if(treatment <= potency && isDangerous) {
+                treatmentsNeeded--;
+            }else if (treatment > potency){
+                treatmentsNeeded++;
+            }
+            if(treatmentsNeeded == 0){
+                isDangerous = false;
+            }
+        }
+
+    }
+
+}
